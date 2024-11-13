4573ad367a88514498bcb3fcfa9a84556ad3c03c
falba3 <99144557+falba3@users.noreply.github.com>
2024-11-13 19:06:18 +0100

    Nov 13 - midi to tensor fix (timesteps x notes)

--- convert_midi.py
+++ convert_midi.py
@@ -1,93 +1,66 @@
 import os
-import numpy as np
 import torch
-import mido
-import pretty_midi
-from torch.utils.data import Dataset, DataLoader
-from torch.nn.utils.rnn import pad_sequence
+from music21 import midi, note, chord
+
+def open_midi(midi_path, remove_drums):
+    # There is an one-line method to read MIDIs
+    # but to remove the drums we need to manipulate some
+    # low level MIDI events.
+    mf = midi.MidiFile()
+    mf.open(midi_path)
+    mf.read()
+    mf.close()
+    if (remove_drums):
+        for i in range(len(mf.tracks)):
+            mf.tracks[i].events = [ev for ev in mf.tracks[i].events if ev.channel != 10]
+
+    return midi.translate.midiFileToStream(mf)
 
 
-# Midi to Piano Roll Function
-def midi_to_piano_roll(file_path, fs=100):
-    midi_data = pretty_midi.PrettyMIDI(file_path)
+def midi_to_binary_tensor(midi_path, remove_drums=True, time_resolution=0.25):
+    """
+    Converts a MIDI file to a binary PyTorch tensor representation.
 
-    # Convert to piano roll (time x notes)
-    piano_roll = midi_data.get_piano_roll(fs=fs)
+    Args:
+    - midi_path: Path to the MIDI file.
+    - remove_drums: Boolean indicating whether to remove drum tracks.
+    - time_resolution: Time resolution in quarter notes (default 0.25 represents sixteenth notes).
 
-    # Convert to binary (1 if note is played, 0 if not)
-    piano_roll_binary = (piano_roll > 0).astype(int)
-    return piano_roll_binary
+    Returns:
+    - A binary PyTorch tensor where:
+      * Rows represent time (in quarter notes at given resolution).
+      * Columns represent pitches (0-127).
+      * Values are binary, 1 for note presence and 0 for absence.
+    """
+    midi_stream = open_midi(midi_path, remove_drums)
+    time_steps = int(midi_stream.flat.highestTime / time_resolution) + 1
+    pitch_range = 128  # MIDI pitch range (0-127)
+
+    # Binary tensor to hold note on/off data
+    tensor = torch.zeros((time_steps, pitch_range), dtype=torch.int8)
+
+    for part in midi_stream.parts:
+        for note_or_chord in part.flat.notes:
+            start_time = int(note_or_chord.offset / time_resolution)
+
+            if isinstance(note_or_chord, note.Note):
+                pitch_value = int(note_or_chord.pitch.ps)
+                tensor[start_time, pitch_value] = 1
+            elif isinstance(note_or_chord, chord.Chord):
+                for pitch in note_or_chord.pitches:
+                    pitch_value = int(pitch.ps)
+                    tensor[start_time, pitch_value] = 1
+
+    return tensor
 
 
-# Piano Roll to Sequence Function
-def roll2sequence(piano_roll, sequence_length=100):
-    # Check if piano roll has enough time steps for at least one sequence
-    if piano_roll.shape[1] < sequence_length:
-        return None
-    piano_roll_sequences = [piano_roll[:, i:i + sequence_length]
-                        for i in range(0, piano_roll.shape[1] - sequence_length, sequence_length)]
-    piano_roll_sequences = np.stack(piano_roll_sequences)
-    return piano_roll_sequences # shape (num_sequences, 128, sequence_length)
-
-# Sequence to Tensor Function
-def roll2tensor(PR_sequences):
-    return torch.tensor(PR_sequences, dtype=torch.float32) # shape (num_sequences, 128, sequence, length)
-
-
-# Music Dataset class
-class MusicDataset(Dataset):
-    def __init__(self, tensor_list):
-        self.data = tensor_list
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        return self.data[idx]
-
-# Padding function
-def pad_collate(batch):
-    # Pads each sequence to the length of the longest sequence in this batch
-    batch = [item.squeeze() for item in batch]  # Remove extra dimensions if needed
-    padded_batch = pad_sequence(batch, batch_first=True, padding_value=0)
-    return padded_batch
-
-def midi_folder_2_loader(midi_folder, batch_size=32):
+def midi_folder_to_tensors(midi_folder_path):
     # List down the midi files of the folder
-    music_list = os.listdir(midi_folder)
-    music_list = [f"{midi_folder}/{p}" for p in music_list]
-
-    # Convert the midi files into piano rolls
-    PR_list = list(map(midi_to_piano_roll, music_list))
-
-    # Convert the piano rolls into sequences
-    sequences = list(filter(None, map(roll2sequence, PR_list)))
+    music_list = os.listdir(midi_folder_path)
+    music_list = [f"{midi_folder_path}/{p}" for p in music_list]
 
     # Convert sequences to tensors
-    tensors = list(map(roll2tensor, sequences))
+    tensors = list(map(midi_to_binary_tensor, music_list))
 
-    # Convert tensors to dataset
-    dataset = MusicDataset(tensors)
+    return tensors
 
-    # Dataset to data loader
-    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=pad_collate)
-
-    return {midi_folder: loader}
-
-
-def midi_folder_2_tensors(midi_folder):
-    # List down the midi files of the folder
-    music_list = os.listdir(midi_folder)
-    music_list = [f"{midi_folder}/{p}" for p in music_list]
-
-    # Convert the midi files into piano rolls
-    PR_list = list(map(midi_to_piano_roll, music_list))
-
-    # Convert the piano rolls into sequences
-    # sequences = list(filter(None, map(roll2sequence, PR_list)))
-    sequences = list( map(roll2sequence, PR_list))
-
-    # Convert sequences to tensors
-    tensors = list(map(roll2tensor, sequences))
-
-    return {midi_folder: tensors}
---
+++ old_files/convert_midi_old.py
@@ -0,0 +1,99 @@
+import os
+import numpy as np
+import torch
+import mido
+import pretty_midi
+from torch.utils.data import Dataset, DataLoader
+from torch.nn.utils.rnn import pad_sequence
+
+
+# Midi to Piano Roll Function
+def midi_to_piano_roll(file_path, fs=100):
+    midi_data = pretty_midi.PrettyMIDI(file_path)
+
+    # Convert to piano roll (time x notes)
+    piano_roll = midi_data.get_piano_roll(fs=fs)
+
+    # Convert to binary (1 if note is played, 0 if not)
+    piano_roll_binary = (piano_roll > 0).astype(int)
+    return piano_roll_binary
+
+
+# Piano Roll to Sequence Function
+def roll2sequence(piano_roll, sequence_length=100):
+    # Check if piano roll has enough time steps for at least one sequence
+    if piano_roll.shape[1] < sequence_length:
+        return None
+    piano_roll_sequences = [piano_roll[:, i:i + sequence_length]
+                        for i in range(0, piano_roll.shape[1] - sequence_length, sequence_length)]
+    piano_roll_sequences = np.stack(piano_roll_sequences)
+    return piano_roll_sequences # shape (num_sequences, 128, sequence_length)
+
+# Sequence to Tensor Function
+def roll2tensor(PR_sequences):
+    return torch.tensor(PR_sequences, dtype=torch.float32) # shape (num_sequences, 128, sequence, length)
+
+
+# Music Dataset class
+class MusicDataset(Dataset):
+    def __init__(self, tensor_list):
+        self.data = tensor_list
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+# Padding function
+def pad_collate(batch):
+    # Pads each sequence to the length of the longest sequence in this batch
+    batch = [item.squeeze() for item in batch]  # Remove extra dimensions if needed
+    padded_batch = pad_sequence(batch, batch_first=True, padding_value=0)
+    return padded_batch
+
+def midi_folder_2_loader(midi_folder, batch_size=32):
+    # List down the midi files of the folder
+    music_list = os.listdir(midi_folder)
+    music_list = [f"{midi_folder}/{p}" for p in music_list]
+
+    # Convert the midi files into piano rolls
+    PR_list = list(map(midi_to_piano_roll, music_list))
+
+    # Convert the piano rolls into sequences
+    sequences = list(filter(None, map(roll2sequence, PR_list)))
+
+    # Convert sequences to tensors
+    tensors = list(map(roll2tensor, sequences))
+
+    # Convert tensors to dataset
+    dataset = MusicDataset(tensors)
+
+    # Dataset to data loader
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, collate_fn=pad_collate)
+
+    return {midi_folder: loader}
+
+
+def midi_folder_2_tensors(midi_folder):
+    # List down the midi files of the folder
+    music_list = os.listdir(midi_folder)
+    music_list = [f"{midi_folder}/{p}" for p in music_list]
+
+    # Convert the midi files into piano rolls
+    PR_list = list(map(midi_to_piano_roll, music_list))
+
+    # Convert the piano rolls into sequences
+    # sequences = list(filter(None, map(roll2sequence, PR_list)))
+    sequences = list( map(roll2sequence, PR_list))
+
+    # Convert sequences to tensors
+    tensors = list(map(roll2tensor, sequences))
+
+    return {midi_folder: tensors}
+
+
+
+
+
+
